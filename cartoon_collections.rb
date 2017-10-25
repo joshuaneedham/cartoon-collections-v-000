99403c1f03d21c4f9d440795005c55625e8d3b4b
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 16:24:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   array.each do |x|
-    x.length > 4# Your code here
+    x.length > 4
+  end# Your code here
 end
 
 def find_the_cheese# code an argument here
