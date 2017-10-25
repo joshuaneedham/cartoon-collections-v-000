075a7ca2f3cedd8cc0ef974aa00afedd06454fa6
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 16:37:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,8 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.each do |x| 
-      x.include?(cheese_types)
+    array.each do |x|
+      if x.include?(cheese_types)
+        puts "#{x}"
     end
 end
