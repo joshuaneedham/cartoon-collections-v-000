60dd7bf81e003f2c13a2497865758be24c070f96
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 16:24:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)# code an argument here
   array.collect { |x| x.capitalize + "!"}# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.each do |x|
+    x.length > 4# Your code here
 end
 
 def find_the_cheese# code an argument here
